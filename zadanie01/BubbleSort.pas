d764c642d06c2a1e4467da3a599fa6cb8fcbbfd2
strek-o <strek.oliwier@gmail.com>
2026-03-19 22:09:31 +0100

    Upload zadanie01-3.0

---
+++ zadanie01/BubbleSort.pas
@@ -0,0 +1,30 @@
+
+Program BubbleSort;
+
+Uses crt;
+
+Type s
+  IntArray = array[1..50] Of integer;
+
+Var 
+  numbers: IntArray;
+  j: integer;
+
+Procedure GenerateIntegers(Var arr: IntArray);
+
+Var i: integer;
+Begin
+  randomize;
+  For i := 1 To 50 Do
+    arr[i] := random(101);
+End;
+
+Begin
+  clrscr;
+  GenerateIntegers(numbers);
+
+  For j:=1 To 50 Do
+    write(numbers[j], ' ');
+  writeln;
+
+End.
