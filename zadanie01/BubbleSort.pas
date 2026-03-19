d08f9f3ff3c73dc419557b5da1bbe417b6bc7ae7
strek-o <strek.oliwier@gmail.com>
2026-03-19 22:20:12 +0100

    Add zadanie01-3.5

--- zadanie01/BubbleSort.pas
+++ zadanie01/BubbleSort.pas
@@ -19,9 +19,25 @@ Begin
     arr[i] := random(101);
 End;
 
+Procedure BubbleSort(Var arr: IntArray);
+
+Var 
+  k, l, temp: integer;
+Begin
+  For k := 1 To 49 Do
+    For l := 1 To 50 - k Do
+      If arr[l] > arr[l+1] Then
+        Begin
+          temp := arr[l];
+          arr[l] := arr[l+1];
+          arr[l+1] := temp;
+        End;
+End;
+
 Begin
   clrscr;
   GenerateIntegers(numbers);
+  BubbleSort(numbers);
 
   For j:=1 To 50 Do
     write(numbers[j], ' ');
